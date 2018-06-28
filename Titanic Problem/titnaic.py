420cffdbb8a45eb351126cde973b149a6943446a
Bhargav061197 <bhargav12oclock@gmail.com>
2018-06-28 21:50:21 +0530

    submit wale

--- Titanic Problem/titnaic.py
+++ Titanic Problem/titnaic.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import csv as csv
+from sklearn.ensemble import RandomForestClassifier
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+
 df=pd.read_csv("train.csv")
 """
 df.plot(x='Pclass',y='Survived',kind='scatter')
@@ -26,7 +28,8 @@ df["Sex"]=df["Sex"].replace(to_replace="female",value=1)
 df["Embarked"]=df["Embarked"].replace(to_replace=['C','S','Q'],value=[1,2,3])
 df.loc[829,"Embarked"]=1
 df.loc[61,"Embarked"]=1
-X=df[['Pclass','Fare','Sex','SibSp','Parch','Embarked']]
+df['Age'].fillna(23.826033707865168, inplace=True)
+X=df[['Pclass','Fare','Sex','SibSp','Parch','Embarked','Age']]
 y=df['Survived']
 
 knn=KNeighborsClassifier(n_neighbors=3)
@@ -36,15 +39,27 @@ df1=pd.read_csv("test.csv")
 df1["Sex"]=df1["Sex"].replace(to_replace="male",value=0)
 df1["Sex"]=df1["Sex"].replace(to_replace="female",value=1)
 df1["Embarked"]=df1["Embarked"].replace(to_replace=['C','S','Q'],value=[1,2,3])
-test=df1[['Pclass','Fare','Sex','SibSp','Parch','Embarked']]
+df1['Age'].fillna(24.10191846522782, inplace=True)
+test=df1[['Pclass','Fare','Sex','SibSp','Parch','Embarked','Age']]
 test.loc[152,"Fare"]=10
 p=knn.predict(test)
-<<<<<<< HEAD
+
+
+random_forest = RandomForestClassifier(n_estimators=100)
+random_forest.fit(X, y)
+klu = random_forest.predict(test)
+
+
+
+
+
+
+
 ids=df1['PassengerId'].values
 submission_file=open('submission.csv','w')
 open_file_object= csv.writer(submission_file)
 open_file_object.writerow(["PassengerId","Survived"])
-open_file_object.writerows(zip(ids, p))
+open_file_object.writerows(zip(ids, klu))#here change klu to p to print  values and change back to klu to print random forest prediction values
 submission_file.close()
 
 """
@@ -55,3 +70,4 @@ print(p)
 for i in range(0,df1["Pclass"].count()):
     prediction=knn.predict([[test.loc[i]["Pclass"],test.loc[i]["Fare"],test.loc[i]["Sex"],test.loc[i]["SibSp"],test.loc[i]["Parch"]]])
     print(test.loc[i]["Pclass"],test.loc[i]["Fare"],test.loc[i]["Sex"],test.loc[i]["SibSp"],test.loc[i]["Parch"],prediction)
+"""
