efbcfc7a36b9dbc37c0da02984525a2d63b3dc83
rahulkrishnan221 <rahulkrishnan221@gmail.com>
2018-06-28 16:15:24 +0530

    fare changes

--- Titanic Problem/titnaic.py
+++ Titanic Problem/titnaic.py
@@ -24,10 +24,11 @@ print(df["Pclass"][df["Fare"]>500])"""
 df["Sex"]=df["Sex"].replace(to_replace="male",value=0)
 df["Sex"]=df["Sex"].replace(to_replace="female",value=1)
 df["Embarked"]=df["Embarked"].replace(to_replace=['C','S','Q'],value=[1,2,3])
+df.loc[df["Fare"]<=50,"Fare"]=0
+df.loc[df["Fare"]>50,"Fare"]=1
 X=df[['Pclass','Fare','Sex','SibSp','Parch']]
 y=df['Survived']
 X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=0)
 knn=KNeighborsClassifier(n_neighbors=3)
 knn.fit(X_train,y_train)
 print(knn.score(X_test,y_test))
-print(df['Sex'].count())
