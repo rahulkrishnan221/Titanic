41a28ebfa355056e2c81bf0568d6cd56e1f67afb
rahulkrishnan221 <rahulkrishnan221@gmail.com>
2018-06-28 17:49:23 +0530

    Final prediction using test

--- Titanic Problem/titnaic.py
+++ Titanic Problem/titnaic.py
@@ -29,3 +29,12 @@ X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=0)
 knn=KNeighborsClassifier(n_neighbors=3)
 knn.fit(X_train,y_train)
 print(knn.score(X_test,y_test))
+df1=pd.read_csv("test.csv")
+df1["Sex"]=df1["Sex"].replace(to_replace="male",value=0)
+df1["Sex"]=df1["Sex"].replace(to_replace="female",value=1)
+df1["Embarked"]=df1["Embarked"].replace(to_replace=['C','S','Q'],value=[1,2,3])
+test=df1[['Pclass','Fare','Sex','SibSp','Parch']]
+test.loc[152,"Fare"]=10
+for i in range(0,df1["Pclass"].count()):
+    prediction=knn.predict([[test.loc[i]["Pclass"],test.loc[i]["Fare"],test.loc[i]["Sex"],test.loc[i]["SibSp"],test.loc[i]["Parch"]]])
+    print(test.loc[i]["Pclass"],test.loc[i]["Fare"],test.loc[i]["Sex"],test.loc[i]["SibSp"],test.loc[i]["Parch"],prediction)
